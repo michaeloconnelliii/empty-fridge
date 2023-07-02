327a061111137da8d057ebaf5e3e0e11e62aa5e3
Michael O'Connell <michaeloconnelliii@gmail.com>
2023-07-01 21:49:41 -0700

    FilterableUserPreferenceTable component converted to TypeScript.

--- empty-my-fridge/src/components/UserPreferenceTable/FilterableUserPreferenceTable.tsx
+++ empty-my-fridge/src/components/UserPreferenceTable/FilterableUserPreferenceTable.tsx
@@ -1,10 +1,12 @@
 import UserPreferenceSearchBar from './UserPreferenceSearchBar';
 import UserPreferenceTable from './UserPreferenceTable';
+import { UserPreferencesInvokeSet } from '@/types'
 
-export default function FilterableUserPreferenceTable({userInput, setUserPreferences}) {
+export default function FilterableUserPreferenceTable({userInput, setUserPreferences}: UserPreferencesInvokeSet) {
     return (
       <div className='container'>
-        <UserPreferenceSearchBar setUserPreferences={setUserPreferences} />
+        <UserPreferenceSearchBar userInput={userInput} 
+                                 setUserPreferences={setUserPreferences} />
         <UserPreferenceTable userInput={userInput} 
                              setUserPreferences={setUserPreferences} />
       </div>
