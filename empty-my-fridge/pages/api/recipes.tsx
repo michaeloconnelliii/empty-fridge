3e363aa02336cd08e008625c0628759d784793cf
Michael O'Connell <michaeloconnelliii@gmail.com>
2023-07-19 18:31:25 -0700

    Static API endpoint added.

---
+++ empty-my-fridge/pages/api/recipes.tsx
@@ -0,0 +1,29 @@
+import { RecipeInput } from '../../typings';
+
+const RECIPES : RecipeInput = [
+  {
+    id: 9271,
+    title: "Mexican Tuna Ceviche",
+    recipe_description: "Prepare a refreshing Mexican-style ceviche using fresh ahi tuna, diced avocado, lemon juice, and savory seasonings.",
+    ingredients_user_has: ["ahi tuna", "avocado", "lemon"],
+    additional_ingredients_needed: ["serrano pepper", "red onion", "cilantro", "tomato"]
+  },
+  {
+    id: 9272,
+    title: "Grilled Tuna Steak with Avocado Salsa",
+    recipe_description: "Grill seasoned tuna steaks to perfection and serve them with a flavorful avocado salsa for a savory Mexican-inspired dish.",
+    ingredients_user_has: ["ahi tuna", "avocado"],
+    additional_ingredients_needed: ["lime juice", "red onion", "tomato", "jalapeno", "cilantro"]
+  },
+  {
+    id: 9273,
+    title: "Tuna Avocado Salad",
+    recipe_description: "Create a delicious and savory salad by combining ahi tuna chunks with fresh avocado, lemon juice, and a variety of mixed greens.",
+    ingredients_user_has: ["ahi tuna", "avocado", "lemon"],
+    additional_ingredients_needed: ["mixed greens", "red onion", "cucumber", "cherry tomatoes", "olive oil"]
+  }
+];
+
+export default function handler(req, res) {
+  res.status(200).json({ recipes: RECIPES });
+}
