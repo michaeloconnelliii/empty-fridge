168bbc7aabf21f21a9a9caa2545f5f3f690b224c
Michael O'Connell <michaeloconnelliii@gmail.com>
2023-07-23 18:50:57 -0700

    Prevent API call when user hasn't added at least one ingredient or preference.

--- empty-my-fridge/src/components/RecipeAccordion/RecipeSearchBar.tsx
+++ empty-my-fridge/src/components/RecipeAccordion/RecipeSearchBar.tsx
@@ -3,6 +3,7 @@ import { RecipeInputs, RecipeInput } from '@/typings';
 
 export default function RecipeSearchBar({recipes, setRecipes, userInput}: RecipeInputs) {
     const [isLoading, setIsLoading] = useState(false);
+    const [isPreferenceOrIngredientEmpty, setIsPreferenceOrIngredientEmpty] = useState(false);
     
     function clearRecipes() {
       setRecipes(() => []);
@@ -10,6 +11,13 @@ export default function RecipeSearchBar({recipes, setRecipes, userInput}: Recipe
 
     async function fetchRecipes() {
       try {
+        // User must input at least one ingredient and preference for API call to be successful
+        if(!userInput.ingredients.length || !userInput.preferences.length) {
+          setIsPreferenceOrIngredientEmpty(true);
+          return;
+        }
+
+        // Make the API call with user's preferences and ingredients
         const response = await fetch('/api/recipes',
                                   {
                                     body: JSON.stringify(userInput),
@@ -18,11 +26,16 @@ export default function RecipeSearchBar({recipes, setRecipes, userInput}: Recipe
         if (!response.ok) {
           throw new Error('Failed to fetch recipes.');
         }
+        
         const data: RecipeInput = await response.json();
+        
+        // Add new recipes to existing recipe output
         setRecipes(prevRecipes => [...prevRecipes, ...data]);
       } catch (error) {
         console.error('Error fetching recipes:', error);
       }
+
+      setIsPreferenceOrIngredientEmpty(false);
     };
   
     async function findRecipes() {
@@ -32,22 +45,25 @@ export default function RecipeSearchBar({recipes, setRecipes, userInput}: Recipe
     }
   
     return (
-      <form className='form-inline justify-content-center mb-4'>
-        <button className={`btn btn-primary mr-2${isLoading ? ' isLoading' : ''} `}
-                type='button'
-                onClick={findRecipes}
-                disabled={isLoading}
-                >
-          <span className={`spinner-grow spinner-grow-sm mr-1 ${isLoading ? '' : 'd-none'}`} 
-                role="status" 
-                aria-hidden="true"></span>
-          {`${isLoading ? 'Loading...' : 'Find Recipes'}`}
-        </button>
-        <button className='btn btn-danger'
-                type='button'
-                onClick={clearRecipes}>
-          Clear Recipes
-        </button>
-      </form>
+      <>
+        <form className='form-inline justify-content-center mb-4'>
+          <button className={`btn btn-primary mr-2${isLoading ? ' isLoading' : ''} `}
+                  type='button'
+                  onClick={findRecipes}
+                  disabled={isLoading}
+                  >
+            <span className={`spinner-grow spinner-grow-sm mr-1 ${isLoading ? '' : 'd-none'}`} 
+                  role="status" 
+                  aria-hidden="true"></span>
+            {`${isLoading ? 'Loading...' : 'Find Recipes'}`}
+          </button>
+          <button className='btn btn-danger'
+                  type='button'
+                  onClick={clearRecipes}>
+            Clear Recipes
+          </button>
+        </form>
+        <div className={`text-center text-danger ${isPreferenceOrIngredientEmpty ? '' : 'd-none'}`}>Please input at least one ingredient and preference.</div>
+      </>
     );
   }
