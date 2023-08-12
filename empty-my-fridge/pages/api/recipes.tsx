3901303f208b43f8ffb2fdb21f4c2021a83fa709
Michael O'Connell <michaeloconnelliii@gmail.com>
2023-08-12 16:48:06 -0700

    Fixing message, removing sample data.

--- empty-my-fridge/pages/api/recipes.tsx
+++ empty-my-fridge/pages/api/recipes.tsx
@@ -6,30 +6,6 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
-const RECIPES : RecipeInput = [
-  {
-    id: 9271,
-    title: "Mexican Tuna Ceviche",
-    recipe_description: "Prepare a refreshing Mexican-style ceviche using fresh ahi tuna, diced avocado, lemon juice, and savory seasonings.",
-    ingredients_user_has: ["ahi tuna", "avocado", "lemon"],
-    additional_ingredients_needed: ["serrano pepper", "red onion", "cilantro", "tomato"]
-  },
-  {
-    id: 9272,
-    title: "Grilled Tuna Steak with Avocado Salsa",
-    recipe_description: "Grill seasoned tuna steaks to perfection and serve them with a flavorful avocado salsa for a savory Mexican-inspired dish.",
-    ingredients_user_has: ["ahi tuna", "avocado"],
-    additional_ingredients_needed: ["lime juice", "red onion", "tomato", "jalapeno", "cilantro"]
-  },
-  {
-    id: 9273,
-    title: "Tuna Avocado Salad",
-    recipe_description: "Create a delicious and savory salad by combining ahi tuna chunks with fresh avocado, lemon juice, and a variety of mixed greens.",
-    ingredients_user_has: ["ahi tuna", "avocado", "lemon"],
-    additional_ingredients_needed: ["mixed greens", "red onion", "cucumber", "cherry tomatoes", "olive oil"]
-  }
-];
-
 async function getRecipes(userInput) {
   userInput = JSON.stringify(userInput);
   
@@ -100,7 +76,7 @@ export default async function handler(req, res) {
         {
           res.status(200).json( recipes );
         } else {
-          res.status(500).send({message: 'Something occured with API. Project maintainer/creator needs to look into what went wrong.'});
+          res.status(500).send({message: 'Something occured with the API. Project maintainer/creator needs to look into what went wrong.'});
         }
         return;
     }
